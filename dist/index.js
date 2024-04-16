2ce07588985c98f6dded8efce4d534cb72205c29
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-16 19:52:22 +0100

    type in typeScript

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
-console.log(Math.floor(10.5));
+let theName = "youssef";
+let age = 40;
+let hire = true;
+let all = theName;
+all = age;
+all = hire;
+function add(n, a) {
+    return a + n;
+}
+console.log(add(1, "3")); // "13"
 //# sourceMappingURL=index.js.map
