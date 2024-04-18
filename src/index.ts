08274938174ef70e109b50b3516ba2023743e2be
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-18 23:29:35 +0100

    updating tsconfig, noImplicitReturns check all code paths in a function to ensure they return a value

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 let showMsg = true;
 
-function showDetails(name, age, salary){
+function showDetails(name: string, age: number, salary: number){
     if(showMsg){
-        return `Hello ${name}, Your Ag is ${age}`;
+        return `Hello ${name}, Your Ag is ${age}, Salary is ${salary}`;
     }
+
+    return "Ther is no data to chow";
 }
 
-console.log(showDetails("Youssef", 16));
+console.log(showDetails("Youssef", 16, 5000));
