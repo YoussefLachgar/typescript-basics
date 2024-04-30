628ff78f7682dae0d4657ac60a2a26213f9211d8
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-30 16:25:56 +0100

    updating tsconfig file to preserve const enums on the compilation

--- dist/index.js
+++ dist/index.js
@@ -1,41 +1,4 @@
 "use strict";
-let showMsg = true;
-function showDetails(name, age, salary) {
-    if (showMsg) {
-        return `Hello ${name}, Your Ag is ${age}`;
-    }
-    return "Ther is no data to chow";
-}
-function addAll(...nums) {
-    let result = 0;
-    nums.forEach((num) => result += num);
-    return result;
-}
-const add = (num1, num2) => num1 + num2;
-let smya = "Elking";
-let jarab;
-jarab = 10;
-jarab = "ra can jarab";
-function getActions(btns) {
-    console.log(`Action for Button up is ${btns.up}`);
-    console.log(`Action for Button down is ${btns.down}`);
-    console.log(`Action for Button right is ${btns.right}`);
-    console.log(`Action for Button left is ${btns.left}`);
-    console.log(`Action for Last type x is ${btns.x}`);
-}
-function compare(num1, num2) {
-    if (num1 === num2) {
-        return 0;
-    }
-    else if (num1 > num2) {
-        return 1;
-    }
-    else {
-        return -1;
-    }
-}
-let article = [11, "title one", true];
-const [id, title, published] = article;
 var Level;
 (function (Level) {
     Level[Level["Kids"] = 15] = "Kids";
@@ -45,6 +8,6 @@ var Level;
 })(Level || (Level = {}));
 let lvl = "Easy";
 if (lvl === "Easy") {
-    console.log(`the level is ${lvl} And number of seconds is ${Level.Easy}`);
+    console.log(`the level is ${lvl} And number of seconds is ${9}`);
 }
 //# sourceMappingURL=index.js.map
